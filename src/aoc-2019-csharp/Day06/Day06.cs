9d46c040e0e9b92aeba7801f7e89a801b5b50b76
Joe Nace <jwnace@gmail.com>
2023-09-25 15:39:33 -0400

    added solution for day 06

--- src/aoc-2019-csharp/Day06/Day06.cs
+++ src/aoc-2019-csharp/Day06/Day06.cs
@@ -2,9 +2,53 @@ namespace aoc_2019_csharp.Day06;
 
 public static class Day06
 {
-    private static readonly string Input = File.ReadAllText("Day06/day06.txt");
+    private static readonly string[] Input = File.ReadAllLines("Day06/day06.txt");
 
-    public static int Part1() => 1;
+    public static int Part1() => CalculateChecksum(Input);
 
-    public static int Part2() => 2;
+    public static int Part2() => CalculateTransfers(Input);
+
+    public static int CalculateChecksum(string[] input)
+    {
+        var checksum = 0;
+        var orbits = input.Select(x => x.Split(")")).ToDictionary(x => x[1], x => x[0]);
+
+        foreach (var orbit in orbits)
+        {
+            var current = orbit.Key;
+
+            while (orbits.ContainsKey(current))
+            {
+                checksum++;
+                current = orbits[current];
+            }
+        }
+
+        return checksum;
+    }
+
+    public static int CalculateTransfers(string[] input)
+    {
+        var directOrbits = input.Select(x => x.Split(")")).ToDictionary(x => x[1], x => x[0]);
+        var yourOrbits = GetOrbits(directOrbits, "YOU");
+        var santasOrbits = GetOrbits(directOrbits, "SAN");
+
+        var closestCommonOrbit = yourOrbits.Intersect(santasOrbits).First();
+
+        return yourOrbits.IndexOf(closestCommonOrbit) + santasOrbits.IndexOf(closestCommonOrbit) - 2;
+    }
+
+    private static List<string> GetOrbits(Dictionary<string, string> orbits, string start)
+    {
+        var you = new List<string>();
+        var current = start;
+
+        while (orbits.ContainsKey(current))
+        {
+            you.Add(current);
+            current = orbits[current];
+        }
+
+        return you;
+    }
 }
--- tests/aoc-2019-csharp-tests/Day06Tests.cs
+++ tests/aoc-2019-csharp-tests/Day06Tests.cs
@@ -5,17 +5,43 @@ namespace aoc_2019_csharp_tests;
 public class Day06Tests
 {
     [Test]
+    public void Part1_Example_ReturnsCorrectAnswer()
+    {
+        var input = new[] { "COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L" };
+        var expected = 42;
+
+        var actual = Day06.CalculateChecksum(input);
+
+        actual.Should().Be(expected);
+    }
+
+    [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 234446;
         var actual = Day06.Part1();
+        actual.Should().Be(expected);
+    }
+
+    [Test]
+    public void Part2_Example_ReturnsCorrectAnswer()
+    {
+        var input = new[]
+        {
+            "COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L", "K)YOU", "I)SAN",
+        };
+
+        var expected = 4;
+
+        var actual = Day06.CalculateTransfers(input);
+
         actual.Should().Be(expected);
     }
 
     [Test]
     public void Part2_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 385;
         var actual = Day06.Part2();
         actual.Should().Be(expected);
     }
