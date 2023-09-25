8de8f30a8ca7fa55b5ac8427e64b1bf0a0ff08a0
Joe Nace <jwnace@gmail.com>
2023-09-25 11:23:40 -0400

    backfilled unit tests for examples

--- src/aoc-2019-csharp/Day03/Day03.cs
+++ src/aoc-2019-csharp/Day03/Day03.cs
@@ -4,28 +4,30 @@ public static class Day03
 {
     private static readonly string[] Input = File.ReadAllLines("Day03/day03.txt");
 
-    public static int Part1()
+    public static int Part1() => GetShortestDistance(Input[0], Input[1]);
+
+    public static int Part2() => GetFewestSteps(Input[0], Input[1]);
+
+    public static int GetShortestDistance(string line1, string line2)
     {
-        var wire1 = GetCoordinates(Input[0]);
-        var wire2 = GetCoordinates(Input[1]);
+        var wire1 = GetCoordinates(line1);
+        var wire2 = GetCoordinates(line2);
 
         return wire1.Intersect(wire2).Min(GetManhattanDistanceFromOrigin);
     }
 
-    public static int Part2()
+    public static int GetFewestSteps(string line1, string line2)
     {
-        var wire1 = GetCoordinates(Input[0]);
-        var wire2 = GetCoordinates(Input[1]);
+        var wire1 = GetCoordinates(line1);
+        var wire2 = GetCoordinates(line2);
 
-        return wire1.Intersect(wire2).Min(c => GetStepsFromOrigin(c, wire1, wire2));
+        return wire1.Intersect(wire2).Min(intersection => GetStepsFromOrigin(intersection, wire1, wire2));
     }
 
     private static List<Coordinate> GetCoordinates(string input)
     {
         var coordinates = new List<Coordinate>();
-
         var instructions = input.Split(',');
-
         var temp = new Coordinate(0, 0);
 
         foreach (var instruction in instructions)
