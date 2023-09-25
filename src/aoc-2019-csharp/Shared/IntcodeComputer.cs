4ef83c3c79c5b664b7914e0eb121a98493d8464c
Joe Nace <jwnace@gmail.com>
2023-09-25 15:04:07 -0400

    cleaned up `IntcodeComputer` implementation to be more readable and maintainable

--- src/aoc-2019-csharp/Shared/IntcodeComputer.cs
+++ src/aoc-2019-csharp/Shared/IntcodeComputer.cs
@@ -37,125 +37,112 @@ public class IntcodeComputer
                 break;
             }
 
-            switch (opcode)
+            i = opcode switch
             {
-                case 1:
-                {
-                    var (aMode, bMode) = (instruction[2], instruction[1]);
-                    var (a, b, c) = (_memory[i + 1], _memory[i + 2], _memory[i + 3]);
-
-                    var aValue = aMode == '0' ? _memory[a] : a;
-                    var bValue = bMode == '0' ? _memory[b] : b;
-
-                    _memory[c] = aValue + bValue;
-
-                    i += 4;
-                    break;
-                }
-                case 2:
-                {
-                    var (aMode, bMode) = (instruction[2], instruction[1]);
-                    var (a, b, c) = (_memory[i + 1], _memory[i + 2], _memory[i + 3]);
-
-                    var aValue = aMode == '0' ? _memory[a] : a;
-                    var bValue = bMode == '0' ? _memory[b] : b;
-
-                    _memory[c] = aValue * bValue;
-
-                    i += 4;
-                    break;
-                }
-                case 3:
-                {
-                    var parameter = _memory[i + 1];
-                    _memory[parameter] = _inputs[0];
-                    _inputs = _inputs[1..];
-                    i += 2;
-                    break;
-                }
-                case 4:
-                {
-                    var aMode = instruction[2];
-
-                    var a = _memory[i + 1];
-
-                    var aValue = aMode == '0' ? _memory[a] : a;
-
-                    _outputs = _outputs.Append(aValue).ToArray();
-                    i += 2;
-                    break;
-                }
-                case 5:
-                {
-                    var (aMode, bMode) = (instruction[2], instruction[1]);
-                    var (a, b) = (_memory[i + 1], _memory[i + 2]);
-
-                    var aValue = aMode == '0' ? _memory[a] : a;
-                    var bValue = bMode == '0' ? _memory[b] : b;
-
-                    if (aValue != 0)
-                    {
-                        i = bValue;
-                    }
-                    else
-                    {
-                        i += 3;
-                    }
-
-                    break;
-                }
-                case 6:
-                {
-                    var (aMode, bMode) = (instruction[2], instruction[1]);
-                    var (a, b) = (_memory[i + 1], _memory[i + 2]);
-
-                    var aValue = aMode == '0' ? _memory[a] : a;
-                    var bValue = bMode == '0' ? _memory[b] : b;
-
-                    if (aValue == 0)
-                    {
-                        i = bValue;
-                    }
-                    else
-                    {
-                        i += 3;
-                    }
-
-                    break;
-                }
-                case 7:
-                {
-                    var (aMode, bMode) = (instruction[2], instruction[1]);
-                    var (a, b, c) = (_memory[i + 1], _memory[i + 2], _memory[i + 3]);
-
-                    var aValue = aMode == '0' ? _memory[a] : a;
-                    var bValue = bMode == '0' ? _memory[b] : b;
-
-                    _memory[c] = aValue < bValue ? 1 : 0;
-
-                    i += 4;
-                    break;
-                }
-                case 8:
-                {
-                    var (aMode, bMode) = (instruction[2], instruction[1]);
-                    var (a, b, c) = (_memory[i + 1], _memory[i + 2], _memory[i + 3]);
-
-                    var aValue = aMode == '0' ? _memory[a] : a;
-                    var bValue = bMode == '0' ? _memory[b] : b;
-
-                    _memory[c] = aValue == bValue ? 1 : 0;
-
-                    i += 4;
-                    break;
-                }
-                default:
-                {
-                    throw new Exception($"Invalid opcode {opcode} at position {i}");
-                }
-            }
+                1 => ProcessAdditionInstruction(instruction, i),
+                2 => ProcessMultiplicationInstruction(instruction, i),
+                3 => ProcessInputInstruction(i),
+                4 => ProcessOutputInstruction(instruction, i),
+                5 => ProcessJumpIfTrueInstruction(instruction, i),
+                6 => ProcessJumpIfFalseInstruction(instruction, i),
+                7 => ProcessLessThanInstruction(instruction, i),
+                8 => ProcessEqualsInstruction(instruction, i),
+                _ => throw new Exception($"Invalid opcode {opcode} at position {i}")
+            };
         }
 
         return _memory[0];
+    }
+
+    private int ProcessAdditionInstruction(string instruction, int i)
+    {
+        var (a, b, c) = (_memory[i + 1], _memory[i + 2], _memory[i + 3]);
+        var (aMode, bMode) = (instruction[2], instruction[1]);
+        var aValue = aMode == '0' ? _memory[a] : a;
+        var bValue = bMode == '0' ? _memory[b] : b;
+
+        _memory[c] = aValue + bValue;
+
+        return i + 4;
+    }
+
+    private int ProcessMultiplicationInstruction(string instruction, int i)
+    {
+        var (a, b, c) = (_memory[i + 1], _memory[i + 2], _memory[i + 3]);
+        var (aMode, bMode) = (instruction[2], instruction[1]);
+        var aValue = aMode == '0' ? _memory[a] : a;
+        var bValue = bMode == '0' ? _memory[b] : b;
+
+        _memory[c] = aValue * bValue;
+
+        return i + 4;
+    }
+
+    private int ProcessInputInstruction(int i)
+    {
+        var a = _memory[i + 1];
+        _memory[a] = _inputs[0];
+        _inputs = _inputs[1..];
+
+        return i + 2;
+    }
+
+    private int ProcessOutputInstruction(string instruction, int i)
+    {
+        var a = _memory[i + 1];
+        var aMode = instruction[2];
+        var aValue = aMode == '0' ? _memory[a] : a;
+
+        _outputs = _outputs.Append(aValue).ToArray();
+
+        return i + 2;
+    }
+
+    private int ProcessJumpIfTrueInstruction(string instruction, int i)
+    {
+        var (a, b) = (_memory[i + 1], _memory[i + 2]);
+        var (aMode, bMode) = (instruction[2], instruction[1]);
+        var aValue = aMode == '0' ? _memory[a] : a;
+        var bValue = bMode == '0' ? _memory[b] : b;
+
+        i = aValue == 0 ? i + 3 : bValue;
+
+        return i;
+    }
+
+    private int ProcessJumpIfFalseInstruction(string instruction, int i)
+    {
+        var (a, b) = (_memory[i + 1], _memory[i + 2]);
+        var (aMode, bMode) = (instruction[2], instruction[1]);
+        var aValue = aMode == '0' ? _memory[a] : a;
+        var bValue = bMode == '0' ? _memory[b] : b;
+
+        i = aValue == 0 ? bValue : i + 3;
+
+        return i;
+    }
+
+    private int ProcessLessThanInstruction(string instruction, int i)
+    {
+        var (a, b, c) = (_memory[i + 1], _memory[i + 2], _memory[i + 3]);
+        var (aMode, bMode) = (instruction[2], instruction[1]);
+        var aValue = aMode == '0' ? _memory[a] : a;
+        var bValue = bMode == '0' ? _memory[b] : b;
+
+        _memory[c] = aValue < bValue ? 1 : 0;
+
+        return i + 4;
+    }
+
+    private int ProcessEqualsInstruction(string instruction, int i)
+    {
+        var (a, b, c) = (_memory[i + 1], _memory[i + 2], _memory[i + 3]);
+        var (aMode, bMode) = (instruction[2], instruction[1]);
+        var aValue = aMode == '0' ? _memory[a] : a;
+        var bValue = bMode == '0' ? _memory[b] : b;
+
+        _memory[c] = aValue == bValue ? 1 : 0;
+
+        return i + 4;
     }
 }
