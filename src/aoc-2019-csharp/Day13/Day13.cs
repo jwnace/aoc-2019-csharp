bdac911f3fa5ae862466324f1b019919440dfb2d
Joe Nace <jwnace@gmail.com>
2023-10-12 14:45:42 -0400

    added solution for part 1

--- src/aoc-2019-csharp/Day13/Day13.cs
+++ src/aoc-2019-csharp/Day13/Day13.cs
@@ -1,10 +1,21 @@
-﻿namespace aoc_2019_csharp.Day13;
+﻿using aoc_2019_csharp.Extensions;
+using aoc_2019_csharp.Shared;
+
+namespace aoc_2019_csharp.Day13;
 
 public static class Day13
 {
-    private static readonly string Input = File.ReadAllText("Day13/day13.txt");
+    private static readonly long[] Input =
+        File.ReadAllText("Day13/day13.txt").Trim().Split(',').Select(long.Parse).ToArray();
 
-    public static int Part1() => 1;
+    public static long Part1()
+    {
+        var computer = new IntcodeComputer(Input, 1000);
+        computer.Run();
+        var outputs = computer.GetOutputs();
 
-    public static int Part2() => 2;
+        return outputs.Chunk(3).Count(x => x[2] == 2);
+    }
+
+    public static int Part2() => throw new NotImplementedException();
 }
