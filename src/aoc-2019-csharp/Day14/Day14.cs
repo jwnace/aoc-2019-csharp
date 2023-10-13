d552580113361dbba9c05a40736c9bea5f49c067
Joe Nace <jwnace@gmail.com>
2023-10-13 13:04:48 -0400

    added solution for day 14

--- src/aoc-2019-csharp/Day14/Day14.cs
+++ src/aoc-2019-csharp/Day14/Day14.cs
@@ -1,10 +1,63 @@
-﻿namespace aoc_2019_csharp.Day14;
+﻿using aoc_2019_csharp.Extensions;
+
+namespace aoc_2019_csharp.Day14;
 
 public static class Day14
 {
-    private static readonly string Input = File.ReadAllText("Day14/day14.txt");
+    private static readonly string[] Input = File.ReadAllLines("Day14/day14.txt");
 
-    public static int Part1() => 1;
+    public static long Part1() => Solve1(Input);
 
-    public static int Part2() => 2;
+    public static int Part2() => Solve2(Input);
+
+    public static long Solve1(string[] input)
+    {
+        var reactions = GetReactions(input);
+        var factory = new Factory(reactions);
+        var ore = factory.GetOreRequirementToProduce(1, "FUEL");
+
+        return ore;
+    }
+
+    public static int Solve2(string[] input)
+    {
+        const long ore = 1_000_000_000_000;
+        var reactions = GetReactions(input);
+        var start = 0;
+        var end = int.MaxValue;
+
+        while (end - start > 1)
+        {
+            var fuel = (start + end) / 2;
+            var factory = new Factory(reactions);
+            var oreRequirement = factory.GetOreRequirementToProduce(fuel, "FUEL");
+
+            if (oreRequirement > ore)
+            {
+                end = fuel;
+            }
+            else
+            {
+                start = fuel;
+            }
+        }
+
+        return start;
+    }
+
+    private static List<Reaction> GetReactions(string[] input)
+    {
+        var reactions = new List<Reaction>();
+
+        foreach (var line in input)
+        {
+            var (left, right) = line.Split(" => ");
+            var inputs = left.Split(", ").Select(x => x.Split(" ")).Select(x => new Chemical(int.Parse(x[0]), x[1]));
+            var output = new Chemical(int.Parse(right.Split(" ")[0]), right.Split(" ")[1]);
+            var reaction = new Reaction(inputs.ToArray(), output);
+            reactions.Add(reaction);
+        }
+
+        return reactions;
+    }
 }
---
+++ src/aoc-2019-csharp/Day14/Reaction.cs
@@ -0,0 +1,3 @@
+namespace aoc_2019_csharp.Day14;
+
+public record Reaction(Chemical[] Inputs, Chemical Output);
