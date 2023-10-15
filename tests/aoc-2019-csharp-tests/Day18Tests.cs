9eef8941b6aa889cbf094bfbedf262fd10951c2a
Joe Nace <jwnace@gmail.com>
2023-10-15 16:17:17 -0400

    added tests for part 1 examples

--- tests/aoc-2019-csharp-tests/Day18Tests.cs
+++ tests/aoc-2019-csharp-tests/Day18Tests.cs
@@ -4,10 +4,64 @@ namespace aoc_2019_csharp_tests;
 
 public class Day18Tests
 {
+    [TestCase(new[]
+    {
+        "#########",
+        "#b.A.@.a#",
+        "#########",
+    }, 8)]
+    [TestCase(new[]
+    {
+        "########################",
+        "#f.D.E.e.C.b.A.@.a.B.c.#",
+        "######################.#",
+        "#d.....................#",
+        "########################",
+    }, 86)]
+    [TestCase(new[]
+    {
+        "########################",
+        "#...............b.C.D.f#",
+        "#.######################",
+        "#.....@.a.B.c.d.A.e.F.g#",
+        "########################",
+    }, 132)]
+    [TestCase(new[]
+    {
+        "#################",
+        "#i.G..c...e..H.p#",
+        "########.########",
+        "#j.A..b...f..D.o#",
+        "########@########",
+        "#k.E..a...g..B.n#",
+        "########.########",
+        "#l.F..d...h..C.m#",
+        "#################",
+    }, 136)]
+    [TestCase(new[]
+    {
+        "########################",
+        "#@..............ac.GI.b#",
+        "###d#e#f################",
+        "###A#B#C################",
+        "###g#h#i################",
+        "########################",
+    }, 81)]
+    public void Part1_Example_ReturnsCorrectAnswer(string[] input, int expected)
+    {
+        Day18.Solve1(input).Should().Be(expected);
+    }
+
     [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
         Day18.Part1().Should().Be(0);
+    }
+
+    [TestCase(new[] { "" }, 0)]
+    public void Part2_Example_ReturnsCorrectAnswer(string[] input, int expected)
+    {
+        Day18.Solve2(input).Should().Be(expected);
     }
 
     [Test]
