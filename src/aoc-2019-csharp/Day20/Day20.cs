80ecc43463b53791ed9c042254c3b8f7812b3e96
Joe Nace <jwnace@gmail.com>
2023-10-16 21:55:01 -0400

    added placeholder for day 20 solution and tests for part 1 examples

--- src/aoc-2019-csharp/Day20/Day20.cs
+++ src/aoc-2019-csharp/Day20/Day20.cs
@@ -2,9 +2,19 @@
 
 public static class Day20
 {
-    private static readonly string Input = File.ReadAllText("Day20/day20.txt");
+    private static readonly string[] Input = File.ReadAllLines("Day20/day20.txt");
 
-    public static int Part1() => 1;
+    public static int Part1() => Solve1(Input);
 
-    public static int Part2() => 2;
+    public static int Part2() => Solve2(Input);
+
+    public static int Solve1(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static int Solve2(string[] input)
+    {
+        throw new NotImplementedException();
+    }
 }
