9f363575e8e3773969d4dfa031776b397d1444d3
Joe Nace <jwnace@gmail.com>
2023-10-21 00:20:36 -0400

    added solution for day 25

--- tests/aoc-2019-csharp-tests/Day25Tests.cs
+++ tests/aoc-2019-csharp-tests/Day25Tests.cs
@@ -7,12 +7,12 @@ public class Day25Tests
     [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
-        Day25.Part1().Should().Be(0);
+        Day25.Part1().Should().Be(134227456);
     }
 
     [Test]
     public void Part2_ReturnsCorrectAnswer()
     {
-        Day25.Part2().Should().Be(0);
+        Day25.Part2().Should().Be("Merry Christmas!");
     }
 }
