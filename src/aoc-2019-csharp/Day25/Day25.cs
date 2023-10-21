8683a28118466358127c5ee7dfb3efcb33200e6b
Joe Nace <jwnace@gmail.com>
2023-10-20 22:56:42 -0400

    added placeholder for day 25 solution

--- src/aoc-2019-csharp/Day25/Day25.cs
+++ src/aoc-2019-csharp/Day25/Day25.cs
@@ -4,7 +4,13 @@ public static class Day25
 {
     private static readonly string Input = File.ReadAllText("Day25/day25.txt");
 
-    public static int Part1() => 1;
+    public static int Part1()
+    {
+        throw new NotImplementedException();
+    }
 
-    public static int Part2() => 2;
+    public static int Part2()
+    {
+        throw new NotImplementedException();
+    }
 }
